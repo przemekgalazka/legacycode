eb731d789dd6cdc15d564798f63b492e6403d2b9
Przemek Gałązka <przemek.galazka@gmail.com>
2013-07-25 11:57:40 +0200

    process message plus testy

--- src/test/java/ccproxy/MessageFromAsteriskTest.java
+++ src/test/java/ccproxy/MessageFromAsteriskTest.java
@@ -3,6 +3,7 @@ package ccproxy;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.fest.assertions.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
@@ -11,14 +12,22 @@ import static org.mockito.Mockito.*;
  */
 public class MessageFromAsteriskTest {
 
-  Parametry parametry;
   MessageFromAsterisk sut;
+  Parametry parametry;
+  Events events;
+
+  AgentConnection agentConnection;
+  WiadomoscOdAsteriska wiadomosc;
 
 
   @Before
   public void setUp() throws Exception {
     parametry = mock(Parametry.class);
-    sut = new MessageFromAsterisk(parametry);
+    events = mock(Events.class);
+    sut = new MessageFromAsterisk(parametry, events);
+
+    agentConnection = mock(AgentConnection.class);
+    wiadomosc = new WiadomoscOdAsteriska();
   }
 
 
@@ -36,23 +45,77 @@ public class MessageFromAsteriskTest {
 
 
   @Test
-  public void shouldProcessAgentLogIn() throws Exception {
+  public void shouldProcessLogin() throws Exception {
+
     //-------------------- GIVEN -------------------------------------------------------------------
-
-
-    //-------------------- WHEN --------------------------------------------------------------------
-    String asteriskResponse = "aa Agent logged in";
-    AgentConnection agentConnection = mock(AgentConnection.class);
-    WiadomoscOdAsteriska wiadomosc = new WiadomoscOdAsteriska();
+    String agentLoggedInResponse = "Agent logged in";
     boolean knownMsg = false;
 
-    boolean actualResult = sut.agentLoggedIn(asteriskResponse, agentConnection,
+    //-------------------- WHEN --------------------------------------------------------------------
+    boolean actualResult = sut.processMessage(agentLoggedInResponse, agentConnection,
         knownMsg, wiadomosc);
 
     //-------------------- THEN --------------------------------------------------------------------
+    assertThat(wiadomosc.getWiadomosc()).isEqualTo("+OK LOGGED IN");
+    verify(events).login(agentConnection);
+    assertThat(actualResult).isEqualTo(true);
+  }
 
 
-//    assertThat(actualResult).isEqualTo(true);
+  @Test
+  public void shouldNotProcessAgentLoginIn() throws Exception {
+    //-------------------- GIVEN -------------------------------------------------------------------
+    String agentLoggedInResponse = "some response";
+    boolean knownMsg = false;
+
+    //-------------------- WHEN --------------------------------------------------------------------
+    boolean actualResult = sut.processMessage(agentLoggedInResponse, agentConnection,
+        knownMsg, wiadomosc);
+
+    //-------------------- THEN --------------------------------------------------------------------
+    assertThat(actualResult).isEqualTo(knownMsg);
 
   }
+
+
+  @Test
+  public void shouldProcessAgentAlreadyLoggedIn() throws Exception {
+
+    //-------------------- GIVEN -------------------------------------------------------------------
+    String agentResponse = "Agent already logged in";
+    boolean knownMsg = false;
+
+    sut = spy(sut);
+    doNothing().when(sut).removeAgentFromQueues(anyInt());
+
+    //-------------------- WHEN --------------------------------------------------------------------
+    boolean actualResult = sut.processMessage(agentResponse, agentConnection, knownMsg, wiadomosc);
+
+    //-------------------- THEN --------------------------------------------------------------------
+    assertThat(wiadomosc.getWiadomosc()).isEqualTo("+OK AGENT ALREADY LOGGED IN");
+    assertThat(actualResult).isEqualTo(true);
+
+    verify(events).login(agentConnection);
+    verify(sut).removeAgentFromQueues(anyInt());
+
+  }
+
+
+  @Test
+  public void shouldProcessAgentLogout() throws Exception {
+
+    //-------------------- GIVEN -------------------------------------------------------------------
+    String agentLoggedInResponse = "Agent logged out";
+    boolean knownMsg = false;
+
+    //-------------------- WHEN --------------------------------------------------------------------
+    boolean actualResult = sut.processMessage(agentLoggedInResponse, agentConnection,
+        knownMsg, wiadomosc);
+
+    //-------------------- THEN --------------------------------------------------------------------
+    assertThat(wiadomosc.getWiadomosc()).isEqualTo("+OK LOGGED OUT");
+    verify(events).logout(agentConnection);
+    assertThat(actualResult).isEqualTo(true);
+  }
+
 }
--- src/test/java/ccproxy/ParametryTest.java
+++
@@ -1,8 +0,0 @@
-package ccproxy;
-
-/**
- * @author Przemysław Gałązka
- * @since 25-07-2013
- */
-public class ParametryTest {
-}
